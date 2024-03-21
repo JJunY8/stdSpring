6a3b80cc355c9f3003c5fb16be76e9d8f4ddbcde
JJunY8 <sksskdi7718@naver.com>
2024-03-21 10:32:35 +0900

    필요없는 폴더 삭제

---
+++ src/main/java/com/joonzero/spring_basic/di/no_di/NoDiMain.java
@@ -0,0 +1,8 @@
+package com.joonzero.spring_basic.di.no_di;
+
+public class NoDiMain {
+    public static void main(String[] args) {
+        Singer singer = new Singer();
+        singer.sing();
+    }
+}
